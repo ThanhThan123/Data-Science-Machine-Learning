ccca30f9f67b91f8baaf555e6689cae41915711c
ThanhThan123 <nguyenthanhthan244@gmail.com>
2026-04-05 17:10:46 +0700

    Tổng quan về Machine Learning và Data Science

---
+++ ex11-Data-Scient.py
@@ -0,0 +1,62 @@
+# Tổng quan về Machine Learning và Data Science
+    # AI: Đưa ra hành động từ dữ liệu
+    # Machine Learning: Học hỏi từ dữ liệu  (CV)
+        # Lập trình tường minh
+            # Rule-based AI : Mô hình dựa vào quy tắc (if - else)
+        # Lập trình không tường minh ( Dùng cho mô hình Machine Learning)
+        # Minh hoạ cho mô hình Machine Learning
+            # Input (feature vector) -> Model -> Output (prediction)
+            # Dataset (Bộ dữ liệu):Thông thường là dạng bảng (Tabular data)
+                # 2 dạng dữ liệu: csv và excel
+                # 1 cột là về thuộc tính
+                # 1 hàng là về giá trị
+            # Feature ( thuộc tính)
+                # Numberical(số)
+                    # Integer (số nguyên)
+                    # float (số thực)
+                # Categorical (phân loại)
+                    # Nomical (định danh)
+                        # Red, Blue, Green
+                    #Ordinal (thứ bậc)
+                        # Happy, Normal, Sad
+                    # Boolean(Logic)
+                        # True, False
+        # Các phương thức học trong Machine-Learning
+            # Supervised learning (Học có giám sát) (Output nó tường minh)
+                # Classification ( Phân loại) (sử dụng dữ liệu có đánh nhãn) Output: Hữu hạn
+                    # Fraud detection
+                    # Email spam detection
+                    # Diagnostíc
+                    # Image classification
+                # Regresstion (Hồi quy) (sử dụng dữ liệu có đánh nhãn) Output: Vô hạn
+                    # Rísk assessment
+                    # Score prediction
+            # Unsupervised learning (Học không giám sát)
+            # Không sử dụng dữ liệu có nhãn
+                # Clustering (Phân cụ) (sử dụng dữ liệu không nhãn):
+                    # Customer clustering
+                    # Data visualization
+                # Association (kết hợp) (sử dụng dữ liệu không nhãn)
+                    # Recommemdation System (Hệ thống gợi ý)
+            # Reinforcement learning ( Huấn luyện bằng agent(Tác nhân))
+            # Tác nhân tương tác với môi trường, môi trường trả về feedback(positive,negative ) và tác nhân sẽ dựa vào
+            # đó để cải thiện độ chính xác
+                # Game theory
+                # Autonomous cars
+        # Model-based AI
+    # Deep Learning : Học hỏi từ rất nhiều từ dữ liệu
+    # Data Science: Tạo ra giá trị từ dữ liệu (Xây dựng prototype) (CV)
+        # Domain expertise : Kiến thức chuyen ngành khi đi làm
+        # Mathematics:
+        # Computer Science:
+# Các mô hình cơ bản (Supervised)
+
+# UnSupervised
+
+#Data visualization
+
+# Model evaluation
+
+# Project
+#
+
